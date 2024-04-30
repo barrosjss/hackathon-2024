d1481c9dfe3ea4f9919c7586be91aa588b56e737
Antonio <antoniojrudass@gmail.com>
2024-04-30 14:43:28 -0500

    update

--- src/components/vendedor/index.tsx
+++ src/components/vendedor/index.tsx
@@ -4,7 +4,6 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 
 const items = [
   {
